b8e82ebe2cf8479ac31f9e99f40d45b6b6e9537e
deadmau915 <mauriciomarinorozco@yahoo.com.co>
2016-05-09 15:42:42 -0500

    primer commint del los analizadores lexico y sontactico de un lnguaje reducido php

---
+++ prueba.php
@@ -0,0 +1,40 @@
+ <?php
+
+ 	echo 'hola mundo';
+
+ 	$variable = 9;
+ 	$variable1 = 3;
+ 	$v1(3); $v2; $v3;
+
+ 	$booleano = true;
+
+ 	$suma = $variable + $variable1;
+
+ 	echo $suma;
+
+    require('conexion.php');
+    
+ 	if($booleano == true)
+		echo $suma;
+		echo $suma;
+		echo $suma;
+		
+	switch($i)
+	{
+		case 1: echo "One"; break;
+		case 3:
+		case 4: echo "Three or Four"; break;
+		case 5: echo "Five"; break;
+		default: echo "Thingy"; break;
+	}
+	
+ 			
+	//esto es un comentario corto 
+	
+	/* 
+	 * esto es un comentario largo deberia 
+	 * reconocerlo todo por saltos de linea 
+	 * que haga 
+	*/
+
+ ?>
